185fa28168186c2bf6d280af97f89aeff115f83d
PetarZerajic <petarzerajic99@gmail.com>
2024-01-24 12:40:19 +0100

    Adding Outlet from react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { Container } from "react-bootstrap";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+
 export default function App() {
   return (
     <>
