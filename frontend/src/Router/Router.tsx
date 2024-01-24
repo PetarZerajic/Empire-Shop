4c9014fb132fe924a632777b9401048c9c669b54
PetarZerajic <petarzerajic99@gmail.com>
2024-01-24 15:39:46 +0100

    Updating data

--- frontend/src/Router/Router.tsx
+++ frontend/src/Router/Router.tsx
@@ -6,12 +6,14 @@ import {
 } from "react-router-dom";
 import App from "../App";
 import { HomeScreen } from "../screens/HomeScreen";
+import { ProductScreen } from "../screens/ProductScreen/ProductScreen";
 import { Routes } from "./Routes";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={Routes.HOME} element={<App />}>
       <Route index={true} path={Routes.HOME} element={<HomeScreen />} />
+      <Route path={Routes.Product} element={<ProductScreen />} />
     </Route>
   )
 );
--- frontend/src/Router/Routes.ts
+++ frontend/src/Router/Routes.ts
@@ -2,6 +2,7 @@ export const Routes = {
   HOME: "/",
   LOGIN: "/login",
   REGISTER: "/register",
+  Product: "/product/:id",
   Products: "/products/:id",
   Cart: "/cart",
 };
