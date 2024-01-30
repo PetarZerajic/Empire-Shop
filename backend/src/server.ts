576db9775b35a9156f72e2aa64affa98231a85c3
PetarZerajic <petarzerajic99@gmail.com>
2024-01-30 21:00:54 +0100

    Updated data in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,54 +1,22 @@
 import express from "express";
 import "dotenv/config.js";
 import { connectToDb } from "../config/db";
-import { Prouduct } from "./models/productModel";
-import { User } from "./models/userModel";
-
+import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
+import { errorController } from "./controllers/errorController";
+import morgan from "morgan";
 const app = express();
 connectToDb();
-
 const port = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
-app.get("/api/products", async (req, res, next) => {
-  try {
-    const products = await Prouduct.find();
-    res.json({
-      status: "success",
-      results: products.length,
-      data: products,
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
-app.get("/api/users", async (req, res, next) => {
-  try {
-    const users = await User.find();
-    res.json({
-      status: "success",
-      results: users.length,
-      data: users,
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
-app.get("/api/product/:id", async (req, res, next) => {
-  try {
-    const product = await Prouduct.findById(req.params.id);
-    res.json({
-      status: "success",
-      data: product,
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+app.use(errorController);
 app.listen(port, () => {
   console.log("Server running on port:" + port);
 });
