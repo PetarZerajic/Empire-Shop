f98271dc7a38ac1556671a4a6ad88ab1dcb73f36
PetarZerajic <petarzerajic99@gmail.com>
2024-01-31 19:59:30 +0100

    Update data in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
-import { Header } from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
 
 export default function App() {
   return (
