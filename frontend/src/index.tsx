b489d0e38892c949d2e31aa5f28f364dbd878f55
PetarZerajic <petarzerajic99@gmail.com>
2024-02-01 10:30:52 +0100

    Putting a react redux provider around application

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { Router } from "./router/router";
+import store from "./redux/store/store";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Router } from "./Router/Router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>
 );
