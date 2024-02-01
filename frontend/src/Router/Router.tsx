981b9aeaacb3770d1d7139dce695b381cf5a9220
PetarZerajic <petarzerajic99@gmail.com>
2024-02-01 10:31:38 +0100

    Updated data

--- frontend/src/Router/Router.tsx
+++ frontend/src/Router/Router.tsx
@@ -4,16 +4,16 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-import App from "../App";
-import { HomeScreen } from "../screens/HomeScreen/HomeScreen";
-import { ProductScreen } from "../screens/ProductScreen/ProductScreen";
-import { Routes } from "./Routes";
+import { Home } from "../pages/home/home";
+import { Routes } from "./routes";
+import { Product } from "../pages/product/product";
+import App from "../app";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={Routes.HOME} element={<App />}>
-      <Route index={true} path={Routes.HOME} element={<HomeScreen />} />
-      <Route path={Routes.Product} element={<ProductScreen />} />
+      <Route index={true} path={Routes.HOME} element={<Home />} />
+      <Route path={Routes.Product} element={<Product />} />
     </Route>
   )
 );
