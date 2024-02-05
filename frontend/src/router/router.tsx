e9b43ee6668d8cd128ad2c8b13714a1b0b3e6f56
PetarZerajic <petarzerajic99@gmail.com>
2024-02-05 13:48:12 +0100

    Implementig Cart route path

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -8,12 +8,14 @@ import { Home } from "../pages/home/home";
 import { Routes } from "./routes";
 import { Product } from "../pages/product/product";
 import App from "../app";
+import { Cart } from "../pages/cart/cart";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={Routes.HOME} element={<App />}>
       <Route index={true} path={Routes.HOME} element={<Home />} />
       <Route path={Routes.Product} element={<Product />} />
+      <Route path={Routes.Cart} element={<Cart />} />
     </Route>
   )
 );
