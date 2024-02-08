c8a20f2b5dce7756c8cb16ce99b105766ea9678b
PetarZerajic <petarzerajic99@gmail.com>
2024-02-08 14:03:02 +0100

    Adding registration to the user

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,15 +1,16 @@
-import { User } from "../models/userModel";
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { User } from "../models/userModel";
+import { createSendToken } from "../utils/createSendToken";
 import { AppError } from "../utils/appError";
 
-export const gettAllUsers = async (
+export const getAllUsers = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
     const users = await User.find();
-
     res.status(200).json({
       status: "success",
       resultrs: users.length,
@@ -20,7 +21,7 @@ export const gettAllUsers = async (
   }
 };
 
-export const getOneUser = async (
+export const getUser = async (
   req: Request,
   res: Response,
   next: NextFunction
@@ -35,6 +36,25 @@ export const getOneUser = async (
       status: "success",
       data: user,
     });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
+
+    createSendToken(user, 201, res);
   } catch (err) {
     next(err);
   }
