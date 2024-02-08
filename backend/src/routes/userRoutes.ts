25c3d5b40ef3df025a3a0ff64083844f3d98feda
PetarZerajic <petarzerajic99@gmail.com>
2024-02-08 13:00:27 +0100

    Creating user routes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,28 @@
 import express from "express";
-import { getOneUser, gettAllUsers } from "../controllers/userController";
+import {
+  getUserProfile,
+  getUser,
+  getAllUsers,
+  registerUser,
+  logInUser,
+  logOutUser,
+  updateUserProfile,
+  deleteUserProfile,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController";
 
 const router = express.Router();
 
-router.get("/", gettAllUsers);
-router.route("/:id").get(getOneUser);
+router.post("/register", registerUser);
+router.post("/login", logInUser);
+router.get("/logut", logOutUser);
+router
+  .route("/profile")
+  .get(getUserProfile)
+  .patch(updateUserProfile)
+  .delete(deleteUserProfile);
+router.route("/").get(getAllUsers);
+router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
 export default router;
