5224f720713dbfcb1077cd5a3d7fc47a74b6930a
PetarZerajic <petarzerajic99@gmail.com>
2024-02-09 18:19:05 +0100

    Adding body and cookie parser

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,8 +5,16 @@ import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
 import { errorController } from "./controllers/errorController";
 import morgan from "morgan";
-const app = express();
+import cookieParser from "cookie-parser";
+
 connectToDb();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "development") {
