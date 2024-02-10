f54190ad64e6dd6bae5766265fb6b5b4a34b10bb
PetarZerajic <petarzerajic99@gmail.com>
2024-02-10 11:56:41 +0100

    Wrapping root component with PersistGate

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { Router } from "./router/router";
-import store from "./redux/store/store";
+import { persistor, store } from "./redux/store/store";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router />
+      <PersistGate loading={null} persistor={persistor}>
+        <Router />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
