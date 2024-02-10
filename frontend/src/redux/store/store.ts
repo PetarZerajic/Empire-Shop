5a5fb28dd11f6594190954ef3d30d9ce715cbaac
PetarZerajic <petarzerajic99@gmail.com>
2024-02-10 11:55:30 +0100

    Adding persistReducer and persistStore

--- frontend/src/redux/store/store.ts
+++ frontend/src/redux/store/store.ts
@@ -1,16 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../slices/apiSlice";
 import cartSliceReducer from "../slices/cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
-const store = configureStore({
+const persistConfig = {
+  key: "products",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartSliceReducer);
+
+export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    cart: cartSliceReducer,
+    cart: persistedReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+    getDefaultMiddleware({ serializableCheck: false }).concat(
+      apiSlice.middleware
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
-export default store;
+export const persistor = persistStore(store);
