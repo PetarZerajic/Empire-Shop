8357c5e5bab8fa7f93a4e0569e8d033efbb575ef
PetarZerajic <petarzerajic99@gmail.com>
2024-02-12 11:24:20 +0100

    Moving registerUser and loginUser in authContorller.ts

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { AppError } from "../utils/appError";
 import { User } from "../models/userModel";
+import { createSendToken } from "../utils/createSendToken";
 
 interface IDecoded {
   userId: string;
@@ -49,4 +50,48 @@ export const admin = async (
     return next(new AppError(401, "Not authorized as admin"));
   }
   next();
+};
+
+export const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
+
+    createSendToken(user, 201, res);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const logInUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new AppError(400, "Please provide valid email or password"));
+    }
+    const user = await User.findOne({
+      email,
+    }).select("+password");
+
+    if (!user || !(await user.correctPassword(password))) {
+      return next(new AppError(401, "Incorect email or password"));
+    }
+
+    createSendToken(user, 200, res);
+  } catch (err) {
+    next(err);
+  }
 };
