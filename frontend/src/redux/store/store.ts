c945ad1288b5a68c25650fe7e954c2749e73eea0
PetarZerajic <petarzerajic99@gmail.com>
2024-02-15 10:17:33 +0100

    Implementing combineReducer  for cart and auth

--- frontend/src/redux/store/store.ts
+++ frontend/src/redux/store/store.ts
@@ -1,27 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../slices/apiSlice";
 import cartSliceReducer from "../slices/cartSlice";
+import authSliceReducer from "../slices/authSlice";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 
-const persistConfig = {
-  key: "products",
+const cartPersistConfig = {
+  key: "cart",
   storage,
 };
 
-const persistedReducer = persistReducer(persistConfig, cartSliceReducer);
+const authPersistConfig = {
+  key: "auth",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  cart: persistReducer(cartPersistConfig, cartSliceReducer),
+  auth: persistReducer(authPersistConfig, authSliceReducer),
+});
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    cart: persistedReducer,
+    reducer: rootReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }).concat(
       apiSlice.middleware
     ),
 });
-
 export type RootState = ReturnType<typeof store.getState>;
 
 export const persistor = persistStore(store);
