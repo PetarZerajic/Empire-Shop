bc84b5a822b7c18ebc92268f7a1c4f457c427381
PetarZerajic <petarzerajic99@gmail.com>
2024-02-18 20:01:03 +0100

    Adding checkout component

---
+++ frontend/src/components/checkout/checkout.tsx
@@ -0,0 +1,52 @@
+import { Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Routes } from "../../router/routes";
+
+interface ICheckout {
+  step1: boolean;
+  step2: boolean;
+  step3?: boolean;
+  step4?: boolean;
+}
+export const Checkout = ({ step1, step2, step3, step4 }: ICheckout) => {
+  return (
+    <Nav className="justify-content-center mb-4">
+      <Nav.Item>
+        {step1 ? (
+          <LinkContainer to={Routes.LOGIN}>
+            <Nav.Link>Log in</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Log in</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step2 ? (
+          <LinkContainer to={Routes.Shipping}>
+            <Nav.Link>Shipping</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Shipping</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step3 ? (
+          <LinkContainer to={Routes.Payment}>
+            <Nav.Link>Payment</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Payment</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step4 ? (
+          <LinkContainer to={Routes.Placeorder}>
+            <Nav.Link>Placeorder</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Placeorder</Nav.Link>
+        )}
+      </Nav.Item>
+    </Nav>
+  );
+};
