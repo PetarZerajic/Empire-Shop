32008443f468aca00d5f0fc8f33cb58b9a7ca793
PetarZerajic <petarzerajic99@gmail.com>
2024-02-19 12:22:07 +0100

    Adding Payment route

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -14,6 +14,7 @@ import { Register } from "../pages/register/register";
 import { Shipping } from "../pages/shipping/shipping";
 import { Protector } from "../utils/protector";
 import { Profile } from "../pages/profile/profile";
+import { Payment } from "../pages/payment/payment";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -30,6 +31,10 @@ const router = createBrowserRouter(
       <Route
         path={Routes.Profile}
         element={<Protector Component={Profile} />}
+      />
+      <Route
+        path={Routes.Payment}
+        element={<Protector Component={Payment} />}
       />
     </Route>
   )
