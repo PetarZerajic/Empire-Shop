ce977be017f80de222d1b153a3229851cb30ae5d
PetarZerajic <petarzerajic99@gmail.com>
2024-02-20 20:30:32 +0100

    Updated schema in orderModel.ts

--- backend/src/models/orderModel.ts
+++ backend/src/models/orderModel.ts
@@ -1,28 +1,95 @@
 import mongoose, { InferSchemaType } from "mongoose";
 
-const orderSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "Order must belong to a user!"],
-  },
-  orderItems: [
-    {
-      name: { type: String, required: [true, "Oreder must have a name"] },
-      quantity: {
-        type: Number,
-        required: [true, "Order must have a quantity"],
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Order must belong to a user!"],
+    },
+    orderItems: [
+      {
+        name: { type: String, required: [true, "Oreder must have a name"] },
+        quantity: {
+          type: Number,
+          required: [true, "Order must have a quantity"],
+        },
+        imageCover: {
+          type: String,
+          required: [true, "Order must have a image"],
+        },
+        price: { type: Number, required: [true, "Order must have a price"] },
       },
-      coverImage: { type: String, required: [true, "Order must have a image"] },
-      price: { type: Number, required: [true, "Order must have a price"] },
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: [true, "Product must belong to a user!"],
+    ],
+    shippingAddress: {
+      address: {
+        type: String,
+        required: [true, "Shipping address must have a address"],
+      },
+      city: {
+        type: String,
+        required: [true, "Shipping address must have a city"],
+      },
+      postalCode: {
+        type: String,
+        required: [true, "Shipping address must have a postal code"],
+      },
+      country: {
+        type: String,
+        required: [true, "Shipping address must have a country"],
       },
     },
-  ],
-});
-type Order = InferSchemaType<typeof orderSchema>;
+    paymentMethod: {
+      type: String,
+      required: [true, "Order must have a payment method"],
+    },
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
+    itemsPrice: {
+      type: Number,
+      required: [true, "Order must have a items price"],
+      default: 0.0,
+    },
+    taxPrice: {
+      type: Number,
+      required: [true, "Order must have a tax price"],
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: [true, "Order must have a  shipping price"],
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: [true, "Order must have a total price"],
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: [true, "Order must have a paid status"],
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: [true, "Order must have a delivered status"],
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+export type Order = InferSchemaType<typeof orderSchema>;
 
 export const Order = mongoose.model<Order>("Order", orderSchema);
