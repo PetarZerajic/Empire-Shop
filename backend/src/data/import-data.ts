156986c207f1ca4b68bfdedf16240298cae48b90
PetarZerajic <petarzerajic99@gmail.com>
2024-02-20 20:35:45 +0100

    Correcting wrong spelling

--- backend/src/data/import-data.ts
+++ backend/src/data/import-data.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import "dotenv/config.js";
 import { products } from "./products";
-import { Prouduct } from "../models/productModel";
+import { Product } from "../models/productModel";
 import { User } from "../models/userModel";
 import { connectToDb } from "../../config/db";
 
@@ -10,7 +10,7 @@ const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
 
 const importData = async () => {
   try {
-    await Prouduct.create(products, { validateBeforeSave: false });
+    await Product.create(products, { validateBeforeSave: false });
     await User.create(users, { validateBeforeSave: false });
     console.log("Data successfully loaded");
   } catch (err) {
@@ -21,7 +21,7 @@ const importData = async () => {
 
 const deleteData = async () => {
   try {
-    await Prouduct.deleteMany();
+    await Product.deleteMany();
     await User.deleteMany();
     console.log("Data successfully deleted");
   } catch (err) {
