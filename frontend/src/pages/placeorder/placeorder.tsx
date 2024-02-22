fa13addf8dde825c69291e7353a4329a96741a4a
PetarZerajic <petarzerajic99@gmail.com>
2024-02-22 17:47:21 +0100

    Creating Placeorder page

---
+++ frontend/src/pages/placeorder/placeorder.tsx
@@ -0,0 +1,154 @@
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
+import { CheckoutSteps } from "../../components/checkout-steps/checkoutSteps";
+import { RootState } from "../../redux/store/store";
+import { Routes } from "../../router/routes";
+import { emptyShippingFields } from "../../utils/emptyShippingFields";
+import { useCreateOrderMutation } from "../../redux/slices/orderApiSlice";
+import { clearCartItems } from "../../redux/slices/cartSlice";
+import { Loader } from "../../components/loader/loader";
+import "./placeorder.css";
+
+export const Placeorder = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state: RootState) => state.reducer.cart);
+  const {
+    shippingAddress,
+    paymentMethod,
+    cartItems,
+    itemsPrice,
+    shippingPrice,
+    taxPrice,
+    totalPrice,
+  } = cart;
+  const [createOrder, { isLoading }] = useCreateOrderMutation();
+  useEffect(() => {
+    if (emptyShippingFields(shippingAddress)) {
+      navigate(Routes.Shipping);
+    }
+  }, [shippingAddress, navigate]);
+
+  const placeOrderHandler = async () => {
+    try {
+      const response = await createOrder({
+        orderItems: cartItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        shippingPrice,
+        taxPrice,
+        totalPrice,
+      }).unwrap();
+      dispatch(clearCartItems());
+
+      navigate(`/order/${response.data.order._id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address: </strong>
+                {shippingAddress.address},{shippingAddress.city},
+                {shippingAddress.postalCode},{shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>Method: </strong>
+                {paymentMethod}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+
+              <ListGroup variant="flush">
+                {cartItems.map((item, index) => (
+                  <ListGroup.Item key={index}>
+                    <Row>
+                      <Col md={1}>
+                        <Image
+                          src={`images/products/${item.imageCover}`}
+                          alt={item.name}
+                          fluid
+                          rounded
+                        />
+                      </Col>
+                      <Col>
+                        <Link to={`/product/${item._id}`}>{item.name}</Link>
+                      </Col>
+                      <Col md={4}>
+                        <p>
+                          {item.quantity} x ${item.price} =
+                          {item.quantity * item.price}
+                        </p>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items:</Col>
+                  <Col>${itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping:</Col>
+                  <Col>${shippingPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax:</Col>
+                  <Col>${taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total:</Col>
+                  <Col>${totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item className="d-flex justify-content-center">
+                <Button
+                  className="m-1"
+                  type="button"
+                  onClick={placeOrderHandler}
+                >
+                  {isLoading ? (
+                    <Loader width={30} height={30} />
+                  ) : (
+                    "Place Order"
+                  )}
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
