33be0ce0d05ce4ede105d84386669609291bca5f
PetarZerajic <petarzerajic99@gmail.com>
2024-02-29 11:29:12 +0100

    Wrapping router with PayPalScriptProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,12 +5,18 @@ import { Router } from "./router/router";
 import { persistor, store } from "./redux/store/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { PersistGate } from "redux-persist/integration/react";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Router />
+        <PayPalScriptProvider
+          deferLoading={true}
+          options={{ clientId: "test" }}
+        >
+          <Router />
+        </PayPalScriptProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
