8ce48e9ea2128bfb0994ed94b36063466fa3a3ce
PetarZerajic <petarzerajic99@gmail.com>
2024-03-04 11:10:51 +0100

    Creating error message component

---
+++ frontend/src/utils/makeErrorMessage.ts
@@ -0,0 +1,24 @@
+import { SerializedError } from "@reduxjs/toolkit";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import { useLocation } from "react-router-dom";
+
+interface IProps {
+  error: FetchBaseQueryError | SerializedError | undefined;
+}
+
+export const MakeErrorMessage = ({ error }: IProps) => {
+  let errMessage;
+  const { pathname } = useLocation();
+
+  if (error && "status" in error) {
+    if (pathname.endsWith("/")) {
+      errMessage = "data" in error ? JSON.stringify(error.data) : error.error;
+    } else {
+      errMessage =
+        "data" in error
+          ? (error.data as { message: string }).message
+          : error.error;
+    }
+  }
+  return { errMessage };
+};
