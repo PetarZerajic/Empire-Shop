ba9eac9df1e9ce1df8021921ed3e4eb1e085db4a
PetarZerajic <petarzerajic99@gmail.com>
2024-03-06 22:12:31 +0100

    Adding myorders and deliver route

--- backend/src/routes/orderRoutes.ts
+++ backend/src/routes/orderRoutes.ts
@@ -2,7 +2,9 @@ import express from "express";
 import {
   addOrderItems,
   getAllOrders,
+  getMyOrders,
   getOrder,
+  updateOrderToDelivered,
   updateOrderToPaid,
 } from "../controllers/orderController";
 import { admin, protect } from "../controllers/authController";
@@ -11,7 +13,9 @@ const router = express.Router();
 
 router.use(protect);
 router.route("/").get(admin, getAllOrders).post(addOrderItems);
+router.route("/myorders").get(getMyOrders);
 router.route("/:id").get(getOrder);
 router.route("/:id/pay").patch(updateOrderToPaid);
+router.route("/:id/deliver").patch(updateOrderToDelivered);
 
 export default router;
