fde6b3926b57e993b0b48d3019fb162fd63f6d2c
PetarZerajic <petarzerajic99@gmail.com>
2024-03-07 11:26:34 +0100

    Passing product property

--- frontend/src/pages/product/product.tsx
+++ frontend/src/pages/product/product.tsx
@@ -12,25 +12,24 @@ import { useParams } from "react-router-dom";
 import { Rating } from "../../components/rating/rating";
 import { Routes } from "../../router/routes";
 import { useGetOneProductQuery } from "../../redux/slices/productsApiSlice";
-import "./product.css";
 import { useState } from "react";
 import { addTocart } from "../../redux/slices/cartSlice";
 import { useDispatch } from "react-redux";
 import { Message } from "../../components/message/message";
 import { MakeErrorMessage } from "../../utils/makeErrorMessage";
+import "./product.css";
 
 export const Product = () => {
   const { id } = useParams();
-  const { data, isSuccess, error } = useGetOneProductQuery(id);
+  const { data: product, isSuccess, error } = useGetOneProductQuery(id);
   const [quantity, setQuantity] = useState<number>(1);
-  const product = data?.data;
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const addToCartHandler = () => {
     const updatedProduct = {
-      ...product,
+      ...product?.data,
       quantity: quantity,
     };
     dispatch(addTocart(updatedProduct));
@@ -48,27 +47,27 @@ export const Product = () => {
           <Row>
             <Col md={5}>
               <Image
-                src={`/images/products/${product.imageCover}`}
-                alt={product.name}
+                src={`/images/products/${product.data.imageCover}`}
+                alt={product.data.name}
                 fluid
               />
             </Col>
             <Col md={4}>
               <ListGroup variant="flush">
                 <ListGroup.Item>
-                  <h3>{product.name}</h3>
+                  <h3>{product.data.name}</h3>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <Rating
-                    value={product.rating}
-                    text={`${product.numReviews} reviews`}
+                    value={product.data.rating!}
+                    text={`${product.data.numReviews} reviews`}
                   />
                 </ListGroup.Item>
                 <ListGroup.Item style={{ fontWeight: "bold" }}>
-                  Price: ${product.price}
+                  Price: ${product.data.price}
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  Description: {product.description}
+                  Description: {product.data.description}
                 </ListGroup.Item>
               </ListGroup>
             </Col>
@@ -79,7 +78,7 @@ export const Product = () => {
                     <Row>
                       <Col>Price:</Col>
                       <Col>
-                        <strong>${product.price}</strong>
+                        <strong>${product.data.price}</strong>
                       </Col>
                     </Row>
                   </ListGroup.Item>
@@ -88,14 +87,14 @@ export const Product = () => {
                       <Col>Status:</Col>
                       <Col>
                         <strong>
-                          {product.countInStock > 0
+                          {product.data.countInStock > 0
                             ? "In Stock"
                             : "Out of Stock"}
                         </strong>
                       </Col>
                     </Row>
                   </ListGroup.Item>
-                  {product.countInStock > 0 && (
+                  {product.data.countInStock > 0 && (
                     <ListGroup.Item>
                       <Row>
                         <Col>Quantity</Col>
@@ -107,7 +106,7 @@ export const Product = () => {
                               setQuantity(+event.target.value)
                             }
                           >
-                            {[...Array(product.countInStock).keys()].map(
+                            {[...Array(product.data.countInStock).keys()].map(
                               (value) => (
                                 <option key={value + 1} value={value + 1}>
                                   {value + 1}
@@ -122,7 +121,7 @@ export const Product = () => {
                   <ListGroup.Item className="m-auto">
                     <Button
                       type="button"
-                      disabled={product.countInStock === 0}
+                      disabled={product.data.countInStock === 0}
                       onClick={() => addToCartHandler()}
                     >
                       Add To Cart
