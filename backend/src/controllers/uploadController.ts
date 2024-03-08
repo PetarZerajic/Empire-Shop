b9c545ce1a912fac3a263d788f61c49b896a5fb2
PetarZerajic <petarzerajic99@gmail.com>
2024-03-08 10:14:11 +0100

    Adding upload path for images

--- backend/src/controllers/uploadController.ts
+++ backend/src/controllers/uploadController.ts
@@ -3,27 +3,41 @@ import multer from "multer";
 import path from "path";
 import { AppError } from "../utils/appError";
 
-const multerStorage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "../frontend/public/images/products");
-  },
-  filename(req, file, cb) {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
-});
-
-const upload = multer({
-  storage: multerStorage,
-});
-
 export const uploadOnePhoto = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  const { category } = req.params;
+  let uploadPath = "";
+
+  if (category === "products") {
+    uploadPath = "../frontend/public/images/products";
+  } else if (category === "users") {
+    uploadPath = "../frontend/public/images/users";
+  } else {
+    return next(new AppError(400, "Invalid category type"));
+  }
+
+  const multerStorage = multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, uploadPath);
+    },
+    filename(req, file, cb) {
+      const originalname = path.basename(
+        file.originalname,
+        path.extname(file.originalname)
+      );
+      const extension = path.extname(file.originalname);
+
+      cb(null, `${originalname}-${Date.now()}${extension}`);
+    },
+  });
+
+  const upload = multer({
+    storage: multerStorage,
+  });
+
   upload.single("image")(req, res, (err) => {
     if (err) {
       return next(new AppError(400, "Upload failed"));
