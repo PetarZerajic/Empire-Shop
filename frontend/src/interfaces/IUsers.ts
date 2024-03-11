74360c288cce81db0bfe1c356163ee232aec316f
PetarZerajic <petarzerajic99@gmail.com>
2024-03-11 11:23:38 +0100

    Creating IUserInfo

--- frontend/src/interfaces/IUsers.ts
+++ frontend/src/interfaces/IUsers.ts
@@ -1,11 +1,15 @@
 export interface IUsers {
+  _id: string;
+  name: string;
+  email: string;
+  role: string;
+  photo: string;
+  active?: boolean;
+  password?: string;
+  passwordConfirm?: string;
+}
+export interface IUserInfo {
   data: {
-    user: {
-      name: string;
-      email: string;
-      role: string;
-      active: boolean;
-      password: string;
-    };
+    user: IUsers;
   };
 }
