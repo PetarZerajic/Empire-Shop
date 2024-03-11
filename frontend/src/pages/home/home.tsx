916ad8f4a8d9559547f67ece8886359c04e7af5e
PetarZerajic <petarzerajic99@gmail.com>
2024-03-11 11:58:38 +0100

    Adding paginate to Home page

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -5,24 +5,35 @@ import { IProducts } from "../../interfaces/IProducts";
 import { Loader } from "../../components/loader/loader";
 import { Message } from "../../components/message/message";
 import { MakeErrorMessage } from "../../utils/makeErrorMessage";
-
+import { useParams } from "react-router-dom";
+import { Paginate } from "../../components/paginate/paginate";
 export const Home = () => {
-  const { data: products, isLoading, error, isSuccess } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const {
+    data: products,
+    isLoading,
+    isSuccess,
+    error,
+  } = useGetProductsQuery(pageNumber);
   const { errMessage } = MakeErrorMessage({ error });
+
   return (
     <>
       {isLoading && <Loader width={100} height={100} />}
       {error && <Message variant="danger">{errMessage}</Message>}
-
-      <h1 style={{ color: "grey" }}>Latest Products</h1>
-      <Row>
-        {isSuccess &&
-          products.data.map((product: IProducts) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <ProductCard product={product} />
-            </Col>
-          ))}
-      </Row>
+      {isSuccess && (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.data.map((product: IProducts) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <ProductCard product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate pages={products.pages} page={products.page} />
+        </>
+      )}
     </>
   );
 };
