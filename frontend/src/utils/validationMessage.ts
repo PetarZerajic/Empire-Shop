c48313aa019994a04372555bfeaf5633ce42a330
PetarZerajic <petarzerajic99@gmail.com>
2024-03-22 23:17:37 +0100

    Creating validation message

---
+++ frontend/src/utils/validationMessage.ts
@@ -0,0 +1,6 @@
+export const validationMessages: { [key: string]: string } = {
+  address: "Address cannot be left blank!",
+  city: "City cannot be left blank!",
+  postalCode: "Postal Code cannot be left blank!",
+  country: "Country cannot be left blank!",
+};
