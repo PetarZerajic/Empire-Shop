e67d7755f0d693338355c97d9ad3e8485ef50eab
PetarZerajic <petarzerajic99@gmail.com>
2024-03-23 23:46:25 +0100

    Adding get top products query

--- frontend/src/redux/slices/productsApiSlice.ts
+++ frontend/src/redux/slices/productsApiSlice.ts
@@ -1,21 +1,36 @@
 import { apiSlice } from "./apiSlice";
-import { PRODUCTS_URL } from "../../constants/constants";
+import { PRODUCTS_URL, UPLOADS_URL } from "../../constants/constants";
+import { IProducts } from "../../interfaces/IProducts";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (build) => ({
-    getProducts: build.query({
-      query: () => ({
+    getProducts: build.query<
+      { data: IProducts[]; pages: number; page: number },
+      string | unknown
+    >({
+      query: ({ keyword, pageNumber }) => ({
         url: PRODUCTS_URL,
+        params: { keyword, pageNumber },
       }),
       keepUnusedDataFor: 5,
     }),
-    getOneProduct: build.query({
+    getOneProduct: build.query<{ data: IProducts }, string | undefined>({
       query: (id) => ({
         url: `${PRODUCTS_URL}/${id}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    getTopProducts: build.query<{ data: IProducts[] }, void>({
+      query: () => ({
+        url: `${PRODUCTS_URL}/top`,
       }),
       keepUnusedDataFor: 5,
     }),
   }),
 });
 
-export const { useGetProductsQuery, useGetOneProductQuery } = productsApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetOneProductQuery,
+  useGetTopProductsQuery,
+} = productsApiSlice;
