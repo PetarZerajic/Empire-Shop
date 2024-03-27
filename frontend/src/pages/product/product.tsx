98bfe0cda86e7969aa6ec60661696ba8f895276e
PetarZerajic <petarzerajic99@gmail.com>
2024-03-27 14:34:41 +0100

    Adding productSummary and productReview components

--- frontend/src/pages/product/product.tsx
+++ frontend/src/pages/product/product.tsx
@@ -1,15 +1,6 @@
-import {
-  Row,
-  Col,
-  Card,
-  Button,
-  Image,
-  ListGroup,
-  Form,
-} from "react-bootstrap";
+import { Row, Col, Image } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { Rating } from "../../components/rating/rating";
 import { Routes } from "../../router/routes";
 import {
   useCreateProductReviewMutation,
@@ -20,9 +11,12 @@ import { addTocart } from "../../redux/slices/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { Message } from "../../components/message/message";
 import { MakeErrorMessage } from "../../utils/makeErrorMessage";
-import "./product.css";
 import { RootState } from "../../redux/store/store";
 import { toast } from "react-toastify";
+import { Meta } from "../../components/meta/meta";
+import { ProductSummaryCenter, ProductSummaryRight } from "./productSummary";
+import { ProductReview } from "./productReview";
+import "./product.css";
 
 export const Product = () => {
   const { id } = useParams();
@@ -39,8 +33,8 @@ export const Product = () => {
     rating: 0,
     comment: "",
   });
-
   const { userInfo } = useSelector((state: RootState) => state.reducer.auth);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -49,8 +43,9 @@ export const Product = () => {
       ...product?.data,
       quantity: inputValues.quantity,
     };
+
     dispatch(addTocart(updatedProduct));
-    navigate("/cart");
+    navigate(Routes.Cart);
   };
 
   const handleChangeRating = (value: number) => {
@@ -92,7 +87,8 @@ export const Product = () => {
     <>
       {error && <Message variant="danger">{errMessage}</Message>}
       {isSuccess && (
-        <>
+        <div>
+          <Meta title={product.data.name} />
           <Link to={Routes.Home} className="btn btn-light my-3">
             Go back
           </Link>
@@ -105,130 +101,32 @@ export const Product = () => {
               />
             </Col>
             <Col md={4}>
-              <ListGroup variant="flush">
-                <ListGroup.Item>
-                  <h3>{product.data.name}</h3>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Rating
-                    value={product.data.rating!}
-                    text={`${product.data.numReviews} reviews`}
-                  />
-                </ListGroup.Item>
-                <ListGroup.Item style={{ fontWeight: "bold" }}>
-                  Price: ${product.data.price}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  Description: {product.data.description}
-                </ListGroup.Item>
-              </ListGroup>
+              <ProductSummaryCenter product={product} />
             </Col>
             <Col md={3}>
-              <Card>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Price:</Col>
-                      <Col>
-                        <strong>${product.data.price}</strong>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Status:</Col>
-                      <Col>
-                        <strong>
-                          {product.data.countInStock > 0
-                            ? "In Stock"
-                            : "Out of Stock"}
-                        </strong>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-                  {product.data.countInStock > 0 && (
-                    <ListGroup.Item>
-                      <Row>
-                        <Col>Quantity</Col>
-                        <Col>
-                          <Form.Control
-                            as="select"
-                            name="quantity"
-                            value={inputValues.quantity}
-                            onChange={handleChange}
-                          >
-                            {[...Array(product.data.countInStock).keys()].map(
-                              (value) => (
-                                <option key={value + 1} value={value + 1}>
-                                  {value + 1}
-                                </option>
-                              )
-                            )}
-                          </Form.Control>
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  )}
-                  <ListGroup.Item className="m-auto">
-                    <Button
-                      type="button"
-                      disabled={product.data.countInStock === 0}
-                      onClick={() => addToCartHandler()}
-                    >
-                      Add To Cart
-                    </Button>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Card>
+              <ProductSummaryRight
+                product={product}
+                inputValues={inputValues}
+                handleChange={handleChange}
+                addToCartHandler={addToCartHandler}
+              />
             </Col>
           </Row>
           <Row className="review">
-            <Col md={6}>
-              <h2>{isReviewAdded ? "Reviews" : "Create Review"}</h2>
-              {userInfo ? (
-                <div className="mt-4">
-                  {!isReviewAdded ? (
-                    <Form onSubmit={handleOnSubmit}>
-                      <Form.Group controlId="rating" className="my-2">
-                        <Form.Label>Rating</Form.Label>
-                        <Rating
-                          value={inputValues.rating}
-                          handleChangeRating={handleChangeRating}
-                        />
-                      </Form.Group>
-                      <Form.Group className="my-2">
-                        <Form.Label>Comment</Form.Label>
-                        <Form.Control
-                          as="textarea"
-                          name="comment"
-                          value={inputValues.comment}
-                          onChange={handleChange}
-                        />
-                      </Form.Group>
-                      <Button disabled={isLoading} type="submit">
-                        Submit
-                      </Button>
-                    </Form>
-                  ) : null}
-                </div>
-              ) : (
-                <Message>
-                  Please <Link to={Routes.Login}>log in</Link> to write a review
-                </Message>
-              )}
-              <ListGroup.Item className="mt-5" variant="flush">
-                {product.data.reviews?.map((review) => (
-                  <ListGroup.Item key={review._id}>
-                    <strong>{review.name}</strong>
-                    <Rating value={review.rating} />
-                    <p>{review.createdAt?.substring(0, 10)}</p>
-                    <p>{review.comment}</p>
-                  </ListGroup.Item>
-                ))}
-              </ListGroup.Item>
+            <Col md={6} className="mt-4">
+              <ProductReview
+                isReviewAdded={isReviewAdded}
+                userInfo={userInfo}
+                inputValues={inputValues}
+                isLoading={isLoading}
+                product={product}
+                handleOnSubmit={handleOnSubmit}
+                handleChangeRating={handleChangeRating}
+                handleChange={handleChange}
+              />
             </Col>
           </Row>
-        </>
+        </div>
       )}
     </>
   );
