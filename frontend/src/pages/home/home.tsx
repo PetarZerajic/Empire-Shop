fc72c891f1b8fb2508b8d01617d49613134805ce
PetarZerajic <petarzerajic99@gmail.com>
2024-03-27 12:00:40 +0100

    Passing keyword prop on Paginate component

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,7 +1,6 @@
 import { Row, Col } from "react-bootstrap";
 import { useGetProductsQuery } from "../../redux/slices/productsApiSlice";
 import { ProductCard } from "../../components/card/productCard";
-import { IProducts } from "../../interfaces/IProducts";
 import { Loader } from "../../components/loader/loader";
 import { Message } from "../../components/message/message";
 import { MakeErrorMessage } from "../../utils/makeErrorMessage";
@@ -9,6 +8,7 @@ import { Link, useParams } from "react-router-dom";
 import { Paginate } from "../../components/paginate/paginate";
 import { CarouselProduct } from "../../components/carousel/carousel";
 import { Routes } from "../../router/routes";
+import { Meta } from "../../components/meta/meta";
 
 export const Home = () => {
   const { keyword, pageNumber } = useParams();
@@ -19,8 +19,10 @@ export const Home = () => {
     error,
   } = useGetProductsQuery({ keyword, pageNumber });
   const { errMessage } = MakeErrorMessage({ error });
+
   return (
     <>
+    <Meta title="Welcome to Empire-Shop"/>
       {keyword ? (
         <Link to={Routes.Home} className="btn btn-light mb-4">
           Go Back
@@ -34,13 +36,13 @@ export const Home = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.data.map((product: IProducts) => (
+            {products.data.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <ProductCard product={product} />
               </Col>
             ))}
           </Row>
-          <Paginate pages={products.pages} page={products.page} role="user" />
+          <Paginate pages={products.pages} page={products.page} role="user" keyword={keyword}/>
         </>
       )}
     </>
