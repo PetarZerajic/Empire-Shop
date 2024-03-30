5b9a09babda25522b05246ce4b7c524281d1d6df
PetarZerajic <petarzerajic99@gmail.com>
2024-03-30 21:31:04 +0100

    Updated data

--- frontend/src/components/container/modalContainer.tsx
+++ frontend/src/components/container/modalContainer.tsx
@@ -1,60 +1,17 @@
-import { ChangeEvent } from "react";
+import {  ReactNode } from "react";
 import Modal from "react-bootstrap/Modal";
-import { IProducts } from "../../interfaces/IProducts";
-import { IUsers } from "../../interfaces/IUsers";
-import { UserBodyContainer } from "./userBodyContainer";
-import { ProductBodyContainer } from "./productBodyContainer";
 
 interface IProps {
   show: boolean;
-  productValues?: IProducts;
-  userValues?: IUsers;
-  isloading: boolean;
+  children: ReactNode
   handleCloseForm(): void;
-  handleChange(event: ChangeEvent): void;
-  handleChangeImage(event: ChangeEvent): void;
-  handleCreateProduct?(): void;
-  handleEditProduct?(id: string): void;
-  handleEditUser?(id: string): void;
 }
 
 export const ModalContainer = (props: IProps) => {
-  const {
-    show,
-    productValues,
-    userValues,
-    isloading,
-    handleCloseForm,
-    handleChange,
-    handleChangeImage,
-    handleCreateProduct,
-    handleEditProduct,
-    handleEditUser,
-  } = props;
-
-  return (
-    <Modal show={show} onHide={handleCloseForm} backdrop="static">
-      {productValues && (
-        <ProductBodyContainer
-          productValues={productValues}
-          isloading={isloading}
-          handleChange={handleChange}
-          handleChangeImage={handleChangeImage}
-          handleCloseForm={handleCloseForm}
-          handleCreateProduct={handleCreateProduct!}
-          handleEditProduct={handleEditProduct!}
-        />
-      )}
-      {userValues && (
-        <UserBodyContainer
-          userValues={userValues}
-          isloading={isloading}
-          handleChange={handleChange}
-          handleChangeImage={handleChangeImage}
-          handleCloseForm={handleCloseForm}
-          handleEditUser={handleEditUser!}
-        />
-      )}
-    </Modal>
-  );
-};
+  const {show, handleCloseForm, children} = props;
+    return (
+      <Modal show={show} onHide={handleCloseForm} backdrop="static">
+        {children}
+      </Modal>
+    );
+  };
