4f26f88428a696ebda00dc826667d368d3417c9b
PetarZerajic <petarzerajic99@gmail.com>
2024-04-03 10:54:12 +0200

    Refactor

--- frontend/src/pages/order/order.tsx
+++ frontend/src/pages/order/order.tsx
@@ -2,53 +2,30 @@ import { useParams } from "react-router-dom";
 import { Row, Col, ListGroup } from "react-bootstrap";
 import { Message } from "../../components/message/message";
 import { Loader } from "../../components/loader/loader";
-import { IOrder } from "../../interfaces/IOrder";
 import { usePayPalScriptReducer } from "@paypal/react-paypal-js";
-import {
-  OnApproveActions,
-  OnApproveData,
-  CreateOrderData,
-  CreateOrderActions,
-} from "@paypal/paypal-js";
-import {
-  useGetOneOrderQuery,
-  useGetPayPalClientIdQuery,
-  usePayOrderMutation,
-  useDeliverOrderMutation,
-} from "../../redux/slices/orderApiSlice";
+import {OnApproveActions, OnApproveData, CreateOrderData, CreateOrderActions,} from "@paypal/paypal-js";
+import {useGetOneOrderQuery, useGetPayPalClientIdQuery, usePayOrderMutation, useDeliverOrderMutation} from "../../redux/slices/orderApiSlice";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store/store";
 import { MakeErrorMessage } from "../../utils/makeErrorMessage";
-import { OrderShipping } from "./orderShipping";
-import { OrderPaymentMethod } from "./orderPaymentMethod";
-import { OrderItems } from "./orderItems";
-import { OrderSummary } from "./orderSummary";
+import { OrderShipping } from "../../components/order/orderShipping";
+import { OrderPaymentMethod } from "../../components/order/orderPaymentMethod";
+import { OrderItems } from "../../components/order/orderItems";
+import { OrderCard } from "../../components/card/order/orderCard";
+
 
 export const Order = () => {
   const { id } = useParams();
-
-  const { data, isLoading, isSuccess, error, refetch } =
-    useGetOneOrderQuery(id);
-
+  const { data, isLoading, isSuccess, error, refetch } = useGetOneOrderQuery(id);
   const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
-
   const { userInfo } = useSelector((state: RootState) => state.reducer.auth);
-
   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
-
-  const orderDetails: IOrder = data?.data?.order;
-
-  const {
-    data: paypal,
-    isLoading: loadingPayPal,
-    error: errorPayPal,
-  } = useGetPayPalClientIdQuery();
-
-  const [deliverOrder, { isLoading: loadingDeliver }] =
-    useDeliverOrderMutation();
-
+  const {data: paypal, isLoading: loadingPayPal, error: errorPayPal} = useGetPayPalClientIdQuery();
+  const [deliverOrder, { isLoading: loadingDeliver }] = useDeliverOrderMutation();
+  const orderDetails = data?.data?.order
+  
   useEffect(() => {
     if (!errorPayPal && !loadingPayPal && paypal?.clientId) {
       enum SCRIPT_LOADING_STATE {
@@ -83,7 +60,7 @@ export const Order = () => {
   ): Promise<void> => {
     return actions.order!.capture().then(async (details) => {
       try {
-        await payOrder({ id, details });
+        await payOrder({ id, details });   
         refetch();
         toast.success("Payment successfull");
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -102,7 +79,7 @@ export const Order = () => {
         purchase_units: [
           {
             amount: {
-              value: orderDetails.totalPrice.toString(),
+              value: orderDetails!.totalPrice.toString(),
             },
           },
         ],
@@ -126,7 +103,7 @@ export const Order = () => {
     <>
       {isLoading && <Loader width={100} height={100} />}
       {error && <Message variant="danger">{errMessage}</Message>}
-      {isSuccess && (
+      {isSuccess &&  orderDetails &&  (
         <Row>
           <Col md={8}>
             <ListGroup variant="flush">
@@ -142,7 +119,7 @@ export const Order = () => {
             </ListGroup>
           </Col>
           <Col md={4}>
-            <OrderSummary orderDetails={orderDetails} userInfo={userInfo} loadingPay={loadingPay} loadingDeliver={loadingDeliver}
+            <OrderCard orderDetails={orderDetails} userInfo={userInfo} loadingPay={loadingPay} loadingDeliver={loadingDeliver}
               isPending={isPending} onApprove={onApprove} createOrder={createOrder} handleDeliverOrder={handleDeliverOrder} onError={onError}
             />
           </Col>
