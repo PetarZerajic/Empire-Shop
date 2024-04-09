c1f2634e891e9d22e41fd8cca3c5e2f5790bfe3c
PetarZerajic <petarzerajic99@gmail.com>
2024-04-09 10:11:29 +0200

    Adding payment by credit card

--- frontend/src/pages/payment/payment.tsx
+++ frontend/src/pages/payment/payment.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import { Form, Col, Button } from "react-bootstrap";
+import { Form,FormCheck, Col, Button } from "react-bootstrap";
 import { FormContainer } from "../../components/container/formContainer";
 import { CheckoutSteps } from "../../components/checkout-steps/checkoutSteps";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,7 +10,7 @@ import { Routes } from "../../router/routes";
 import { emptyShippingFields } from "../../utils/emptyShippingFields";
 
 export const Payment = () => {
-  const { shippingAddress, paymentMethod } = useSelector(
+  const { shippingAddress } = useSelector(
     (state: RootState) => state.reducer.cart
   );
   const navigate = useNavigate();
@@ -22,6 +22,7 @@ export const Payment = () => {
     }
   }, [shippingAddress, navigate]);
 
+  const [paymentMethod, setPaymentMethod] = useState("");
   const [checked, setChecked] = useState(false);
 
   const onSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
@@ -32,29 +33,46 @@ export const Payment = () => {
 
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
-    dispatch(savePaymentMethod(value));
+    setPaymentMethod(value)
     setChecked(true);
   };
+
+  const styles = {cursor: "pointer"}
+
   return (
     <FormContainer>
       <CheckoutSteps step1 step2 step3 />
       <h1>Payment Method</h1>
       <Form onSubmit={onSubmitHandler} className="mt-3">
-        <Form.Group>
-          <Form.Label as="legend">Select Method</Form.Label>
-          <Col>
-            <Form.Check
+        <Form.Group >
+          <Form.Label className="mb-4" as="legend">Select Method</Form.Label>
+          <Col >
+          <FormCheck>
+            <FormCheck.Input 
               id="paypal"
+              style={styles}
               type="radio"
-              className="my-2"
-              label="Paypal or Credit Card"
-              value={paymentMethod}
-              onChange={onChangeHandler}
-            />
+              value="PayPal"
+              checked={paymentMethod === "PayPal"}  
+              onChange={onChangeHandler}/>
+            <FormCheck.Label htmlFor="paypal" style={styles}> PayPal</FormCheck.Label>
+          </FormCheck>
+
+          <FormCheck className="mt-3">
+            <FormCheck.Input 
+              id="credict-card"
+              style={styles}
+              type="radio"
+              value="Credit Card"
+              checked={paymentMethod === "Credit Card"}  
+              onChange={onChangeHandler}/>
+              <FormCheck.Label htmlFor="credict-card" style={styles}> Credit Card</FormCheck.Label>
+          </FormCheck>
           </Col>
         </Form.Group>
+
         <Button
-          className="mt-2"
+          className="mt-4"
           type="submit"
           variant={checked ? "primary" : "secondary"}
           disabled={!checked}
