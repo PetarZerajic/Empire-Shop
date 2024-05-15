1a47647c578c7912f073cb7cc6c43cb68b6a81c9
PetarZerajic <petarzerajic99@gmail.com>
2024-05-15 17:18:41 +0200

    Refactor

--- frontend/src/components/forms/profile/userProfileForm.tsx
+++ frontend/src/components/forms/profile/userProfileForm.tsx
@@ -5,38 +5,34 @@ import { IUserProfile } from "../../../interfaces/IUsers";
 import { Loader } from "../../loader/loader";
 
 interface IProps {
-  submitHandler(event: FormEvent): void;
+  onSubmitHandler(event: FormEvent): void;
   imgRef: RefObject<HTMLImageElement>;
   inputValues: IUserProfile;
-  loadingUpdateProfile: boolean;
-  loadingUpdatePassword: boolean;
+  loadingProfile: boolean;
+  loadingPassword: boolean;
   handleChangeImage(event: ChangeEvent<HTMLInputElement>): void;
   onChangeHandler(event: ChangeEvent<HTMLInputElement>): void;
 }
 
 export const UserProfileForm = (props: IProps) => {
-  const {
-    submitHandler,
-    imgRef,
-    inputValues,
-    handleChangeImage,
-    onChangeHandler,
-    loadingUpdateProfile,
-    loadingUpdatePassword,
-  } = props;
+  const isLoading = props.loadingProfile || props.loadingPassword;
   return (
-    <Form onSubmit={submitHandler}>
+    <Form onSubmit={props.onSubmitHandler}>
       <Form.Group className="d-flex justify-content-center align-items-center gap-4">
         <img
           id="custom-profile-img"
-          ref={imgRef}
-          src={`/images/users/${inputValues.photo}`}
-          alt={inputValues.name}
+          ref={props.imgRef}
+          src={`/images/users/${props.inputValues.photo}`}
+          alt={props.inputValues.name}
         />
         <Form.Label htmlFor="file" id="file-label">
           <BsUpload />
         </Form.Label>
-        <Form.Control type="file" id="file" onChange={handleChangeImage} />
+        <Form.Control
+          type="file"
+          id="file"
+          onChange={props.handleChangeImage}
+        />
       </Form.Group>
       <Form.Group className="my-2">
         <Form.Label>Name</Form.Label>
@@ -44,8 +40,8 @@ export const UserProfileForm = (props: IProps) => {
           type="text"
           placeholder="Enter name"
           name="name"
-          value={inputValues.name}
-          onChange={onChangeHandler}
+          value={props.inputValues.name}
+          onChange={props.onChangeHandler}
           autoComplete="off"
         />
       </Form.Group>
@@ -55,8 +51,8 @@ export const UserProfileForm = (props: IProps) => {
           type="email"
           placeholder="Enter email"
           name="email"
-          value={inputValues.email}
-          onChange={onChangeHandler}
+          value={props.inputValues.email}
+          onChange={props.onChangeHandler}
           autoComplete="off"
         />
       </Form.Group>
@@ -67,8 +63,8 @@ export const UserProfileForm = (props: IProps) => {
           type="password"
           placeholder="Enter new password"
           name="passwordCurrent"
-          value={inputValues.passwordCurrent}
-          onChange={onChangeHandler}
+          value={props.inputValues.passwordCurrent}
+          onChange={props.onChangeHandler}
           autoComplete="off"
         />
       </Form.Group>
@@ -78,8 +74,8 @@ export const UserProfileForm = (props: IProps) => {
           type="password"
           placeholder="Enter password"
           name="password"
-          value={inputValues.password}
-          onChange={onChangeHandler}
+          value={props.inputValues.password}
+          onChange={props.onChangeHandler}
           autoComplete="off"
         />
       </Form.Group>
@@ -89,18 +85,14 @@ export const UserProfileForm = (props: IProps) => {
           type="password"
           placeholder="Confirm password"
           name="passwordConfirm"
-          value={inputValues.passwordConfirm}
-          onChange={onChangeHandler}
+          value={props.inputValues.passwordConfirm}
+          onChange={props.onChangeHandler}
           autoComplete="off"
         />
       </Form.Group>
 
       <Button type="submit" id="custom-btn" className="mt-3">
-        {loadingUpdateProfile || loadingUpdatePassword ? (
-          <Loader width={30} height={30} />
-        ) : (
-          "Update"
-        )}
+        {isLoading ? <Loader width={30} height={30} /> : "Update"}
       </Button>
     </Form>
   );
